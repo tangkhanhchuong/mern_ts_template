c2c4975bc7b090d479ed51b7263604c007064c0a
tangkhanhchuong <chuongtangkhanh2104@gmail.com>
2021-08-31 20:40:47 +0700

    Resolve merge conflict

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -17,10 +17,8 @@ app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use("/storage", express.static(__dirname + "/storage"))
-<<<<<<< HEAD
+
 const server: Server = http.createServer(app)
-=======
->>>>>>> master
 
 import { AuthRouter, ProductRouter } from "./route"
 app.use("/auth", AuthRouter)
